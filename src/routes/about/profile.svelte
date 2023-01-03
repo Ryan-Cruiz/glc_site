79f48946913e0d11acc295eaa4c12c0be8627e83
ryan <ryancruiz97@gmail.com>
2023-01-04 02:22:58 +0800

    about and activities page added

---
+++ src/routes/about/profile.svelte
@@ -0,0 +1,10 @@
+<div class="col-12 m-2 mx-auto">
+    <div class="card h-100 bg bg-transparent">
+        <p class="card-title text-center p-2">
+            <slot name="name"><span class='text-bold'>Unknown</span></slot>
+        </p>
+        <p class="card-footer text-center">
+            <slot name="position"><span></span></slot>
+        </p>
+    </div>
+</div>
---
+++ src/routes/activities/card-template.svelte
@@ -0,0 +1,15 @@
+<div class="col">
+    <div class="card h-100">
+        <slot name='img'>
+            <img src="https://user-images.githubusercontent.com/43302778/106805462-7a908400-6645-11eb-958f-cd72b74a17b3.jpg" class="card-img-top" alt=''>
+        </slot>
+        <div class="card-body">
+            <slot name='title'>
+                <h5 class="card-title">Nothing</h5>
+            </slot>
+            <slot name='text'>
+                <p class="card-text">Something...</p>
+            </slot>
+        </div>
+    </div>
+</div>
